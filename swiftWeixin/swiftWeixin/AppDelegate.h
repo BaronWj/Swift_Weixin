f3523054e7627cf6b8a05f1338cbee35c7e3c1f2
panweijie <panweijie@okjiaoyu.cn>
2017-08-31 15:03:32 +0800

    创建工程

---
+++ swiftWeixin/swiftWeixin/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  swiftWeixin
+//
+//  Created by 潘伟杰 on 2017/8/31.
+//  Copyright © 2017年 pwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ swiftWeixin/swiftWeixin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  swiftWeixin
+//
+//  Created by 潘伟杰 on 2017/8/31.
+//  Copyright © 2017年 pwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
