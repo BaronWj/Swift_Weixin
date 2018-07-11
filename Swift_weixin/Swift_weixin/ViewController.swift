221d40d1ae4bade1dc6a43dc24a5d9e36717c0b4
panweijie <panweijie@okjiaoyu.cn>
2018-07-11 10:01:30 +0800

    提交配置

--- Swift_weixin/Swift_weixin/ViewController.swift
+++ Swift_weixin/Swift_weixin/ViewController.swift
@@ -14,9 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
-        
-        
+        let name = "小明";
+        print(name);
     }
 
     override func didReceiveMemoryWarning() {
